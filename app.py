7a94dd1e6b860d5628ff31b1ce38a2299569922d
KOguy64 <Rainkunakorn@gmail.com>
2024-11-18 17:14:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ custom_css = f"""
             }}
     </style> """
 
-data = st_canvas(update_streamlit=False, key="png_export", background_image="test.png")
+data = st_canvas(update_streamlit=False, key="png_export", background_image=Image.open("test.png"))
 if data is not None and data.image_data is not None:
     img_data = data.image_data
     im = Image.fromarray(img_data.astype("uint8"), mode="RGBA")
